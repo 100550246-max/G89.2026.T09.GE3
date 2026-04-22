35bbc71979b41e348a18d7f909cf4db63d06d749
100550257 <100550257@alumnos.uc3m.es>
2026-04-22 12:40:03 +0200

    Updated query_date

---
+++ src/main/python/uc3m_consulting/attributes/query_date.py
@@ -0,0 +1,18 @@
+import re
+from datetime import datetime
+from uc3m_consulting.enterprise_management_exception import EnterpriseManagementException
+from uc3m_consulting.attributes.attribute import Attribute
+
+class QueryDate(Attribute):
+    """Class representing and validating a date for queries."""
+
+    def _validate(self, value: str):
+        """Validates the date format for history queries."""
+        date_format = re.compile(r"^(([0-2]\d|3[0-1])\/(0\d|1[0-2])\/\d\d\d\d)$")
+        if not date_format.fullmatch(value):
+            raise EnterpriseManagementException("Invalid date format")
+
+        try:
+            datetime.strptime(value, "%d/%m/%Y").date()
+        except ValueError as ex:
+            raise EnterpriseManagementException("Invalid date format") from ex
