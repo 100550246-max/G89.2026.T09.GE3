6c5beafefcd1b1adc5d56dcf65195bc1eb42368d
100550257 <100550257@alumnos.uc3m.es>
2026-04-22 12:22:49 +0200

    Design: Apply Singleton pattern to ReportJsonStore
    - Implemented __new__ method for ReportJsonStore.
    - Added private __instance attribute to ensure single instantiation.

--- src/main/python/uc3m_consulting/storage/report_json_store.py
+++ src/main/python/uc3m_consulting/storage/report_json_store.py
@@ -3,6 +3,13 @@ from uc3m_consulting.enterprise_manager_config import TEST_NUMDOCS_STORE_FILE
 
 class ReportJsonStore(JsonStore):
     """Specialized store for reports."""
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super(ReportJsonStore, cls).__new__(cls)
+        return cls.__instance
+
     def __init__(self):
         super().__init__()
         self._file_name = TEST_NUMDOCS_STORE_FILE
