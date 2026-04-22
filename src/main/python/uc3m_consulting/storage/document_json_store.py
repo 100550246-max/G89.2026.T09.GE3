e853a3ef455d65ab1bfdbb0e54b6d195a0d29ed4
100550257 <100550257@alumnos.uc3m.es>
2026-04-22 12:18:10 +0200

    Design: Apply Singleton pattern to DocumentJsonStore
    - Implemented __new__ method for DocumentJsonStore.
    - Added private __instance attribute to ensure single instantiation.

--- src/main/python/uc3m_consulting/storage/document_json_store.py
+++ src/main/python/uc3m_consulting/storage/document_json_store.py
@@ -3,6 +3,13 @@ from uc3m_consulting.enterprise_manager_config import TEST_DOCUMENTS_STORE_FILE
 
 class DocumentJsonStore(JsonStore):
     """Specialized store for test documents."""
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super(DocumentJsonStore, cls).__new__(cls)
+        return cls.__instance
+
     def __init__(self):
         super().__init__()
         self._file_name = TEST_DOCUMENTS_STORE_FILE
