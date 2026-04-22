10c07185ecf68fd7768c1dc51479cacd07928828
100550257 <100550257@alumnos.uc3m.es>
2026-04-22 12:17:00 +0200

    Design: Apply Singleton pattern to ProjectJsonStore
    - Implemented __new__ method for ProjectJsonStore.
    - Added private __instance attribute to ensure single instantiation.

--- src/main/python/uc3m_consulting/storage/project_json_store.py
+++ src/main/python/uc3m_consulting/storage/project_json_store.py
@@ -3,6 +3,13 @@ from uc3m_consulting.enterprise_manager_config import PROJECTS_STORE_FILE
 
 class ProjectJsonStore(JsonStore):
     """Specialized store for projects."""
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super(ProjectJsonStore, cls).__new__(cls)
+        return cls.__instance
+
     def __init__(self):
         super().__init__()
         self._file_name = PROJECTS_STORE_FILE
